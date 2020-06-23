9354c55028827577ceaafa56ae772ccc348a4651
Yash Jain <44024753+mvnsia@users.noreply.github.com>
2020-06-24 01:00:32 +0530

    final

---
+++ AR-GREETINGCARD/Assets/DataFiles/Scripts/MenuSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void greetingcard()
+    {
+        SceneManager.LoadScene("AR-GREETING");
+    }
+
+    public void rotatecube()
+    {
+        SceneManager.LoadScene("RotateCube");
+    }
+
+    public void exitbtn()
+    {
+        Application.Quit();
+        Debug.Log("exit button pressed");
+    }
+
+    public void backbtn()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
